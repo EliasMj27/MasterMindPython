c9f34d1b1771aeade8297e6192a61ba2984d4c8b
MasterE08 <98223615+MasterE08@users.noreply.github.com>
2024-04-26 14:24:39 +0200

    Start push 1

--- MasterMind.py
+++ MasterMind.py
@@ -1,1 +1,28 @@
-test
+#Imports
+import os
+
+#Class Screen
+class Screen:
+    def __init__(self):
+        deafultline=""
+        self.lines=[];
+    def clear():
+        os.system('cls');
+#Class Level
+class Level:
+    def __init__(self):
+        self.Position=0;
+        self.Values=[0, 0, 0, 0];
+    def setColor(self,x):
+        self.Values[self.Position]+=x;
+        if self.Values[self.Position] >8:
+            self.Values[self.Position]=0;
+        if self.Values[self.Position] <0:
+            self.Values[self.Position]=8;
+    
+    def ChangePosition(self,x):
+        self.Position+=x;
+        if self.Position<4:
+            self.Position=0;
+        if self.Position<0:
+            self.Position=4
